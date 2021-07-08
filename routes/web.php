3dab1c044932e0c3ea4e3a2eb50b00f82599768f
ripon <riponahmed2201@gmail.com>
2021-07-09 00:37:30 +0600

    simple fix

--- routes/web.php
+++ routes/web.php
@@ -4,20 +4,20 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/','AdminAuthController@showLoginForm');
-route::any('/admin/logout','adminauthcontroller@logout')->name('admin.logout');
-route::post('/admin/login','adminauthcontroller@login')->name('admin.login');
+route::any('/admin/logout','AdminAuthController@logout')->name('admin.logout');
+route::post('/admin/login','AdminAuthController@login')->name('admin.login');
 
 Route::middleware('admin')->group(function(){
     Route::get('/dashboard','HomeController@index');
-    route::get('/customer/list','customercontroller@customerlist')->name('customer.list');
-    route::get('/coupon','couponcontroller@couponlist')->name('coupon.list');
-    route::get('/assign/coupon','couponcontroller@assigncoupon')->name('assign.coupon');
-    route::get('/vbr/list','vbrcontroller@vbrlist')->name('vbr.list');
-    route::post('/vbr/update-status','vbrcontroller@updateVbrStatus')->name('update.vbrStaus');
-    route::get('/create/vbr','vbrcontroller@createVbr')->name('create.vbr');
-    route::post('/add/vbr','vbrcontroller@addVbr')->name('add.vbr');
+    route::get('/customer/list','Customercontroller@customerlist')->name('customer.list');
+    route::get('/coupon','CouponController@couponlist')->name('coupon.list');
+    route::get('/assign/coupon','CouponController@assigncoupon')->name('assign.coupon');
+    route::get('/vbr/list','VbrController@vbrlist')->name('vbr.list');
+    route::post('/vbr/update-status','VbrController@updateVbrStatus')->name('update.vbrStaus');
+    route::get('/create/vbr','VbrController@createVbr')->name('create.vbr');
+    route::post('/add/vbr','VbrController@addVbr')->name('add.vbr');
     Route::get('/report','ReportController@report')->name('report');
-    route::get('/vbr/report','reportcontroller@vbrreport')->name('vbr.report');
+    route::get('/vbr/report','ReportController@vbrreport')->name('vbr.report');
 });
 
 ///
@@ -27,8 +27,8 @@ Route::middleware('admin')->group(function(){
 Route::middleware('vbr')->group(function(){
     Route::get('/vbr/dashboard','HomeController@vbrDashboard');
     Route::get('/my/customer','VbrController@myCustomer')->name('mycustomer');
-    route::get('/generate/coupon','vbrcontroller@coupongenerate')->name('coupon.generate');
-    route::get('/create/customer','vbrcontroller@createCustomer')->name('create.customer');
-    Route::post('/add/customer','vbrcontroller@addCustomer')->name('add.customer');
+    route::get('/generate/coupon','VbrController@coupongenerate')->name('coupon.generate');
+    route::get('/create/customer','VbrController@createCustomer')->name('create.customer');
+    Route::post('/add/customer','VbrController@addCustomer')->name('add.customer');
 });
 
