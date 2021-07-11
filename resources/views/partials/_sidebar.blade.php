8456dcd4a5601da85ba4e574ae8abafb4e310c6b
ripon <riponahmed2201@gmail.com>
2021-07-11 18:27:10 +0600

    commit sidebar issue

--- resources/views/partials/_sidebar.blade.php
+++ resources/views/partials/_sidebar.blade.php
@@ -1,6 +1,6 @@
 <aside class="main-sidebar sidebar-light-purple elevation-4">
     <!-- Brand Logo -->
-    <a href="admin_dashboard.html" class="brand-link">
+    <a href="javascript:void(0)" class="brand-link">
         <img src="{{asset('logo/food_panda_logo.jpg')}}" alt="logo" class="brand-image img-circle elevation-3" style="opacity: .8">
         <span class="brand-text font-weight-light"> &nbsp; VBR</span>
     </a>
