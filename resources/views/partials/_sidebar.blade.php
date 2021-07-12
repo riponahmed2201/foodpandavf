7b744eac4791a46e2ca427a801317f2ae24a9d37
ripon <riponahmed2201@gmail.com>
2021-07-13 00:21:52 +0600

    logo issue fix

--- resources/views/partials/_sidebar.blade.php
+++ resources/views/partials/_sidebar.blade.php
@@ -1,9 +1,9 @@
 <aside class="main-sidebar sidebar-light-pink elevation-4">
     <!-- Brand Logo -->
-    <a href="javascript:void(0)" class="brand-link">
-        <img src="{{asset('logo/food_panda_logo.jpg')}}" alt="logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-        <span class="brand-text font-weight-light"> &nbsp; VBR</span>
-    </a>
+       <a href="javascript:void(0)" >
+           <img style="width: 250px; height: 56px;" src="{{asset('logo/food_panda_logo.jpg')}}" alt="logo">
+{{--           <span class="brand-text font-weight-light"> &nbsp; VBR</span>--}}
+       </a>
     <!-- Sidebar -->
     <div class="sidebar">
         <!-- Sidebar user panel (optional) -->
--- resources/views/partials/_vbr_sidebar.blade.php
+++ resources/views/partials/_vbr_sidebar.blade.php
@@ -1,8 +1,8 @@
 <aside class="main-sidebar sidebar-light-pink elevation-4">
     <!-- Brand Logo -->
-    <a href="javascript:void(0)" class="brand-link">
-        <img src="{{asset('logo/food_panda_logo.jpg')}}" alt="logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-        <span class="brand-text font-weight-light">&nbsp; VBR</span>
+    <a href="javascript:void(0)">
+        <img style="width: 250px; height: 56px;" src="{{asset('logo/food_panda_logo.jpg')}}" alt="logo">
+{{--        <span class="brand-text font-weight-light">&nbsp; VBR</span>--}}
     </a>
     <!-- Sidebar -->
     <div class="sidebar">
